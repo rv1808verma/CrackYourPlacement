808a9314f2a1cf42ee6f2f45f732f829a0e421b5
rv1808verma <pwarmy131@gmail.com>
2024-07-16 14:09:21 +0530

    Create 5. Move Zeroes.cpp

---
+++ Day 1/5. Move Zeroes.cpp
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/move-zeroes/description/
+
+    void moveZeroes(vector<int>& nums) {
+        int i=0, j=0;
+        while(j<nums.size()){
+            if(nums[j]!=0){
+                swap(nums[i],nums[j]);
+                i++;
+            } 
+            j++;
+        }
+    }
