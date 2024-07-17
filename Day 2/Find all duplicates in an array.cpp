8d15f56e64c2b332c963cd64208ecccb74465303
rv1808verma <pwarmy131@gmail.com>
2024-07-17 13:31:45 +0530

    Create Find all duplicates in an array.cpp

---
+++ Day 2/Find all duplicates in an array.cpp
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/find-all-duplicates-in-an-array/
+
+    vector<int> findDuplicates(vector<int>& nums) {
+        map<int,int>mp;
+        for(int i=0; i<nums.size(); i++) mp[nums[i]]++;
+
+        vector<int>ans;
+        for(auto it:mp){
+            if(it.second>1) ans.push_back(it.first);
+        }   
+        return ans;
+    }
