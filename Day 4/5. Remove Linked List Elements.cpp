d0fe403640f61366738f10d2bb6c56d3f6f5de48
rv1808verma <pwarmy131@gmail.com>
2024-07-19 11:55:45 +0530

    Create 5. Remove Linked List Elements.cpp

---
+++ Day 4/5. Remove Linked List Elements.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/remove-linked-list-elements/
+
+    ListNode* removeElements(ListNode* head, int val) {
+        if(head==NULL) return head;
+        while(head!=NULL && head->val==val) head=head->next;
+        ListNode* temp=head;
+        while(temp!=NULL && temp->next!=NULL){
+            if(temp->next->val==val) temp->next=temp->next->next;
+            else temp=temp->next;
+        }
+        temp=head;
+        return temp;
+    }
