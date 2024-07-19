81a243b25419c8ba9177721c1aba6e05d18f82c2
rv1808verma <pwarmy131@gmail.com>
2024-07-19 11:32:57 +0530

    Create 4. Remove Duplicates from a sorted List.cpp

---
+++ Day 1/Day 4/4. Remove Duplicates from a sorted List.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* temp=head;
+        while(temp!=NULL && temp->next!=NULL){
+            if(temp->val==temp->next->val){
+                ListNode* duplicate=temp->next;
+                temp->next=temp->next->next;
+                delete duplicate;
+            }else{
+                temp=temp->next;
+            }
+        }
+        temp=head;
+        return temp;
+    }
