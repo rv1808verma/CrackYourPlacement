48f187800a3c47c762d702b1a5b647771241cdc8
rv1808verma <pwarmy131@gmail.com>
2024-07-25 21:01:48 +0530

    Create Excel Sheet Column Title.cpp

---
+++ Day 10/Excel Sheet Column Title.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/excel-sheet-column-title/
+
+    string convertToTitle(int n) {
+        string ans = "";
+        while(n>0){
+            n--;
+            int currentdigit = n%26;
+            n = n/26;
+            ans.push_back(currentdigit+'A');
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;    
+    }
