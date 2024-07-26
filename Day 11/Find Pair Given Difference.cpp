baebc63e7c32e537a33a1766d18cbd1fc64ad610
rv1808verma <pwarmy131@gmail.com>
2024-07-26 18:30:55 +0530

    Create Find Pair Given Difference.cpp

---
+++ Day 11/Find Pair Given Difference.cpp
@@ -0,0 +1,13 @@
+// https://www.geeksforgeeks.org/problems/find-pair-given-difference1559/1
+
+    int findPair(int n, int x, vector<int> &arr) {
+        // code here
+        sort(arr.begin(), arr.end());
+        int i = 0, j = 1;
+        while (i < n && j < n){
+            if (i != j && arr[j] - arr[i] == x) return 1;
+            else if (arr[j] - arr[i] < x) j++;
+            else i++;
+        }
+        return -1;
+    }
