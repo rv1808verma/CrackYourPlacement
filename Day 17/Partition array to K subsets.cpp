b7df6c8420d942ea31b019577a4aeffa77b75fb0
rv1808verma <pwarmy131@gmail.com>
2024-08-01 07:34:49 +0530

    Create Partition array to K subsets.cpp

---
+++ Day 17/Partition array to K subsets.cpp
@@ -0,0 +1,26 @@
+// https://www.geeksforgeeks.org/problems/partition-array-to-k-subsets/1
+
+    bool isKPartitionPossible(int a[], int n, int k)
+    {
+         //Your code here
+        int total = accumulate(a, a+n, 0);
+        int partition = total/k;
+        if(partition*k != total) return false;
+        sort(a, a+n);
+        if(a[n-1] > partition) return false;
+        
+        priority_queue<int> pq;
+        while(k--) pq.push(0);
+        
+        for(int i = n-1; i >= 0; i--){
+            int x = pq.top(); pq.pop();
+            pq.push(x-a[i]);
+            if(-1*(x-a[i]) > partition) return false;
+        }
+        
+        while(!pq.empty()){
+            if(-pq.top() != partition) return false;
+            pq.pop();
+        }
+        return true;
+    }
