c8a81248f45537e46f332d257d3daf4d7aa6a01c
rv1808verma <pwarmy131@gmail.com>
2024-08-02 06:02:38 +0530

    Create Diameter of a Binary Tree.cpp

---
+++ Day 18/Diameter of a Binary Tree.cpp
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/diameter-of-binary-tree/description/
+
+    int height(TreeNode* node, int& diameter) {
+        if (!node) return 0;
+        int lh = height(node->left, diameter);
+        int rh = height(node->right, diameter);
+        diameter = max(diameter, lh + rh);
+        return 1 + max(lh, rh);
+    }
+
+    int diameterOfBinaryTree(TreeNode* root) {
+        int diameter = 0;
+        height(root, diameter);
+        return diameter;
+    }
