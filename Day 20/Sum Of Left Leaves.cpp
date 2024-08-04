3f4fbd055daf887bc8a6431515e22b0c32df6c54
rv1808verma <pwarmy131@gmail.com>
2024-08-04 07:25:02 +0530

    Create Sum Of Left Leaves.cpp

---
+++ Day 20/Sum Of Left Leaves.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/sum-of-left-leaves/
+
+void solve(TreeNode* &root,int &ans){
+    if(root==NULL) return;
+    
+        if(root->left!=NULL){
+            if(root->left->left==NULL && root->left->right==NULL) 
+            ans=ans+root->left->val;
+    
+        } 
+        solve(root->left,ans);
+        solve(root->right,ans);
+}
+    int sumOfLeftLeaves(TreeNode* root) {
+        int ans=0;
+        solve(root,ans);
+        return ans;
+    }
