572a24359aff35b07baa7a29516aef323aefc8b9
rv1808verma <pwarmy131@gmail.com>
2024-08-05 06:13:33 +0530

    Create Check whether BST contains Dead end.cpp

---
+++ Day 21/Check whether BST contains Dead end.cpp
@@ -0,0 +1,18 @@
+// https://www.geeksforgeeks.org/problems/check-whether-bst-contains-dead-end/1
+
+    bool helper(Node* root,set<int>st){
+        if(root==NULL) return false;
+        if(root->left==NULL && root->right==NULL){
+            if(st.find(root->data-1)!=st.end() && st.find(root->data+1)!=st.end()) 
+                return true;
+        }
+        st.insert(root->data);
+        return helper(root->left,st) || helper(root->right,st);
+    }
+    bool isDeadEnd(Node *root)
+    {
+        set<int>st;
+        st.insert(0);
+        return helper(root,st);
+        
+    }
