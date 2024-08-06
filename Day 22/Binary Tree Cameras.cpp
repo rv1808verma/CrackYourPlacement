b480969f05c49243b2c665e57103da73e999f7a0
rv1808verma <pwarmy131@gmail.com>
2024-08-06 05:47:08 +0530

    Create Binary Tree Cameras.cpp

---
+++ Day 22/Binary Tree Cameras.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/binary-tree-cameras/description/
+
+    int total = 0;
+    int minCameraCover(TreeNode* root) {
+        int val =  postorder(root);
+        if(val==1) total++;
+
+        return total;
+    }
+    int postorder(TreeNode* root){
+        if(!root) return 3;
+
+        int left = postorder(root->left);
+        int right  = postorder(root->right);
+
+        if(left==1||right==1){
+            total++;
+            return 2;
+        }
+        if(left==3&&right==3){
+            return 1;
+        }
+        if(left==2||right==2){
+            return 3;
+        }
+        return 0;
+
+    }
