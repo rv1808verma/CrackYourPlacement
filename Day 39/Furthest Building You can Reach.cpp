3560f2c6c6dd74eef3632b65dccc815633f55bb7
rv1808verma <pwarmy131@gmail.com>
2024-08-23 20:37:00 +0530

    Create Furthest Building You can Reach.cpp

---
+++ Day 39/Furthest Building You can Reach.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/furthest-building-you-can-reach/description/
+
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        
+        for (int i = 0; i < heights.size() - 1; ++i) {
+            int diff = heights[i + 1] - heights[i];
+            
+            if (diff > 0) {
+                pq.push(diff);
+                
+                if (pq.size() > ladders) {
+                    int smallest_diff = pq.top();
+                    pq.pop();
+                    bricks -= smallest_diff;
+                    
+                    if (bricks < 0) {
+                        return i;
+                    }
+                }
+            }
+        }
+        return heights.size() - 1;    
+    }
