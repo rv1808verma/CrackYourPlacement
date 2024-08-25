4fdc6dd7d027357134361fc9abd630e129d7ad38
rv1808verma <pwarmy131@gmail.com>
2024-08-25 06:39:50 +0530

    Create Maximum Product Subarray.cpp

---
+++ Day 41/Maximum Product Subarray.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/maximum-product-subarray/description/
+
+    int maxProduct(vector<int>& nums) {
+        int n=nums.size();
+        int prefix=1, suffix=1;
+        int ans=INT_MIN;
+
+        for(int i=0; i<n; i++){
+            if(prefix==0) prefix=1;
+            if(suffix==0) suffix=1;
+
+            prefix=prefix*nums[i];
+            suffix=suffix*nums[n-i-1];
+            ans=max(ans,max(prefix,suffix));
+        }
+        return ans;    
+    }
