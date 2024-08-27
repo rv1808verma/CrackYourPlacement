0dcb5081532466a94a2215964a509216dc5167e9
rv1808verma <pwarmy131@gmail.com>
2024-08-27 09:20:00 +0530

    Create Delete and Earn.cpp

---
+++ Day 43/Delete and Earn.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/delete-and-earn/description/
+
+    int deleteAndEarn(vector<int>& nums) {
+        unordered_map<int, int> ma;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            ma[nums[i]]++;
+        }
+        vector<int> unq;
+        for (auto it : ma) {
+            unq.push_back(it.first);
+        }
+        sort(unq.begin(), unq.end());
+        vector<int> dp(unq.size() + 1);
+        dp[0] = 0;
+        dp[1] = ma[unq[0]] * unq[0];
+        for (int i = 2; i <= unq.size(); i++) {
+            if (unq[i - 2] < unq[i - 1] - 1) {
+                dp[i] = dp[i - 1] + unq[i - 1] * ma[unq[i - 1]];
+            } else {
+                dp[i] = max(dp[i - 1], dp[i - 2] + unq[i - 1] * ma[unq[i - 1]]);
+            }
+        }
+        return dp[unq.size()];   
+    }
