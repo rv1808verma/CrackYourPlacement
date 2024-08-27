a6549bd4a993244be05dd174c0aed54b6d642343
rv1808verma <pwarmy131@gmail.com>
2024-08-27 09:56:51 +0530

    Create Minimum Insertions to make a string Palindrome.cpp

---
+++ Day 43/Minimum Insertions to make a string Palindrome.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/minimum-insertion-steps-to-make-a-string-palindrome/description/
+
+    int minInsertions(string s) {
+        int n=s.length();
+        string ans=s;
+
+        reverse(ans.begin(),ans.end());
+        vector<vector<int>> dp(n+1, vector<int>(n+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s[i-1]==ans[j-1])
+                    dp[i][j]=1+dp[i-1][j-1];
+                else
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        int len=dp[n][n];
+
+        return n-len;    
+    }
